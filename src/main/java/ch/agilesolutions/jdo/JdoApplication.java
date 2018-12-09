d53bbbc6e9a671669f939d3dfdfc994537749716
Robert Rong <robert.rong@agile-solutions.ch>
2018-12-09 15:25:40 +0100

    f

--- src/main/java/ch/agilesolutions/jdo/JdoApplication.java
+++ src/main/java/ch/agilesolutions/jdo/JdoApplication.java
@@ -28,6 +28,14 @@ public class JdoApplication {
 			profileRepository.save(new Profile("crm", "srp74943lx", "uat"));
 			profileRepository.save(new Profile("crm", "srp10577lx", "sit"));
 			profileRepository.save(new Profile("crm", "srp34766lx", "dev"));
+			profileRepository.save(new Profile("ear", "srp76591lx", "prd"));
+			profileRepository.save(new Profile("ear", "srp73684lx", "uat"));
+			profileRepository.save(new Profile("ear", "srp15437lx", "sit"));
+			profileRepository.save(new Profile("ear", "srp17464lx", "dev"));
+			profileRepository.save(new Profile("jbt", "srp75635lx", "prd"));
+			profileRepository.save(new Profile("jbt", "srp72673lx", "uat"));
+			profileRepository.save(new Profile("jbt", "srp74673lx", "sit"));
+			profileRepository.save(new Profile("jbt", "srp17633lx", "dev"));
 			
 		};
 	}	
