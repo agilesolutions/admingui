875f2235d8f3dd53f0942aaf4bd5ab4955fea311
Robert Rong <robert.rong@agile-solutions.ch>
2018-12-24 11:39:04 +0100

    f

--- src/main/js/logic/Logic.js
+++ src/main/js/logic/Logic.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MessageDialog, { openDialog } from '../dialogs/MessageDialog';
+import Store from '../data/Store';
 
 //https://github.com/MoonHighway/learning-react/blob/master/chapter-11/company-website/src/index.js
 //https://stackoverflow.com/questions/43842793/basic-authentication-with-fetch
@@ -19,6 +20,12 @@ export const deploy = () =>
 	formdata.append('password','qawsedrf');
 	
 	// headers.append('Authorization', 'Basic ' + base64.encode(username + ":" + password));
+	
+	<Store.Consumer>
+	{(name) = () => (
+			console.log(name)
+	)}
+	</Store.Consumer>
 	
     fetch('services/startjob/' + name)
     	.catch(err => console.error(err));
